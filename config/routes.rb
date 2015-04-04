4dfe93a11e52a6ec6a7d9329e7549cab44b95924
Brian Arpie <brian@crisply.com>
2015-04-04 02:10:17 -0400

    setup new route for new controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'holding/prices'
+
+  scope '/api' do
+    get 'holding/prices'
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
