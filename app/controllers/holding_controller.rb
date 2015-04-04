2410a27ef511dfb749c04c297ac2043bea6290ed
Brian Arpie <brian@crisply.com>
2015-04-04 15:03:46 -0400

    refactor filter_by_datE

--- app/controllers/holding_controller.rb
+++ app/controllers/holding_controller.rb
@@ -8,22 +8,24 @@ class HoldingController < ApplicationController
     @end_date = params[:end_date]
 
     holding = Holding.where('id = ? OR name = ?', @id, @ticker_symbol)
-    prices = holding[0].historical_prices
+    @prices = holding[0].historical_prices
 
-    if @start_date && @end_date
-      @start_date = Date.parse @start_date
-      @end_date = Date.parse @end_date
-      prices = prices.where(price_date: @start_date..@end_date)
-    end
+    @prices = @start_date && @end_date ? filter_by_date : @prices
 
     respond_to do |format|
       format.json {
-        render json: prices
+        render json: @prices
       }
       format.any {
         render :status => 500
       }
     end
+  end
+
+  private
+
+  def filter_by_date
+    @prices.where(price_date: @start_date..@end_date)
   end
 
 end
