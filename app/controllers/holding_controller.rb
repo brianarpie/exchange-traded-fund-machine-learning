ee45b19d2774f7e8a30dd378143965f5ce54343b
Brian Arpie <brian@crisply.com>
2015-04-04 02:11:29 -0400

    init the Holding controller with action: prices

--- app/controllers/holding_controller.rb
+++ app/controllers/holding_controller.rb
@@ -1,4 +1,14 @@
 class HoldingController < ApplicationController
   def prices
+    @id = params[:id] || 0
+    @start_date = params[:start_date]
+    @end_date = params[:end_date]
+
+    respond_to do |format|
+      format.json { 
+        data = Holding.where(id: @id)
+        render json: data
+      }
+    end
   end
 end
--- spec/controllers/holding_controller_spec.rb
+++ spec/controllers/holding_controller_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe HoldingController, type: :controller do
+  let(:holding) { FactoryGirl.create(:holding) }
+  let(:historical_price) { FactoryGirl.create(:historical_price) }
 
   describe "GET #prices" do
-    it "returns http success" do
-      get :prices
+    it "returns http json success" do
+      get :prices, format: :json
       expect(response).to have_http_status(:success)
+    end
+
+    it "returns all the historical prices" do
+      holding
+      historical_price
+
+      get :prices, { id: holding.id, format: :json }
+
+      assert_equal 1, JSON.parse(response.body).length
     end
   end
 
