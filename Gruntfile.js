e2a63b4b85ecae516b6adf5d5d3b1795b453fbcb
buwuwei <brian.arpie@gmail.com>
2015-04-13 19:48:46 -0400

    setup basic grunt file for future use

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+(function() {
+  
+  "use strict";
+  
+  module.exports = function(grunt) {
+
+    grunt.config.init({
+
+      jshint: {
+
+      },
+      concat: {
+
+      },
+      uglify: {
+
+      },
+      karma: {
+        unit: {
+          configFile: 'karma.conf.js'
+        }
+      },
+      browserify: {
+
+      }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-browserify');
+
+    grunt.registerTask('test', ['concat', 'karma']);
+
+    grunt.registerTask('default', ['jshint', 'uglify', 'browserify']);
+  
+  }
+
+})();
