6d343da44ca15cc52ecad8f279257a27f1805b60
buwuwei <brian.arpie@gmail.com>
2015-04-17 00:40:59 -0400

    remove setting id as default for factories

--- spec/factories/etf_holdings.rb
+++ spec/factories/etf_holdings.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
   factory :etf_holding do
 
-    id 1
     etf_id 1
     holding_id 1
     
--- spec/factories/etfs.rb
+++ spec/factories/etfs.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
   factory :etf do
-    id 1
     name 'DIV'
   end
 end
