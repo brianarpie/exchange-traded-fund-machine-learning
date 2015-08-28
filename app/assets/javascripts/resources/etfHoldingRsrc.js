9b0741ee42ac6b052148eecc38a89613d844c2b2
Brian Arpie <brian.arpie@gmail.com>
2015-08-28 00:16:28 -0400

    add etf holding resource to frontend app

---
+++ app/assets/javascripts/resources/etfHoldingRsrc.js
@@ -0,0 +1,30 @@
+(function() {
+  
+  "use strict";
+
+  var app = angular.module("ApiResources");
+
+  app.factory("EtfHoldingRsrc", [
+    "$resource",
+    "$cacheFactory",
+    function($resource, $cacheFactory){
+      var etfHoldingRsrc;
+      var route = "/api/etf_holding.json";
+      var cache = $cacheFactory.get("EtfHoldingRsrc");
+
+      if ( !cache ) {
+        cache = $cacheFactory("EtfHoldingRsrc");
+      }
+
+      etfHoldingRsrc = $resource(route, {}, {
+        'get': {
+          method: 'get',
+          cache: cache
+        }
+      });
+
+      return etfHoldingRsrc;
+
+  }]);
+
+})();
